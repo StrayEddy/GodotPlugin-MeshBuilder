edc47b7828cc7a0fd45a099a3bbcd8ebc90701c5
Edouard Murat <edouardmurat1@gmail.com>
2022-09-22 23:37:04 -0400

    Fix #17

--- addons/meshbuilder/mesh_builder_mesh/mesh_builder_mesh.gd
+++ addons/meshbuilder/mesh_builder_mesh/mesh_builder_mesh.gd
@@ -13,6 +13,14 @@ func init(params=["BoxMesh",[],false, 0]):
 			self.mesh.subdivide_width = params[1][1]
 			self.mesh.subdivide_height = params[1][2]
 			self.mesh.subdivide_depth = params[1][3]
+		"CapsuleMesh":
+			self.mesh = CapsuleMesh.new()
+			if params[1].is_empty():
+				params[1] = [.5,2,64,8]
+			self.mesh.radius = params[1][0]
+			self.mesh.height = params[1][1]
+			self.mesh.radial_segments = params[1][2]
+			self.mesh.rings = params[1][3]
 		"CylinderMesh":
 			self.mesh = CylinderMesh.new()
 			if params[1].is_empty():
@@ -24,6 +32,24 @@ func init(params=["BoxMesh",[],false, 0]):
 			self.mesh.rings = params[1][4]
 			self.mesh.cap_top = params[1][5]
 			self.mesh.cap_bottom = params[1][6]
+		"PlaneMesh":
+			self.mesh = PlaneMesh.new()
+			if params[1].is_empty():
+				params[1] = [[2,2],0,0,[0,0,0],1]
+			self.mesh.size = Vector2(params[1][0][0],params[1][0][1])
+			self.mesh.subdivide_width = params[1][1]
+			self.mesh.subdivide_depth = params[1][2]
+			self.mesh.center_offset = Vector3(params[1][3][0],params[1][3][1],params[1][3][2])
+			self.mesh.orientation = params[1][4]
+		"PrismMesh":
+			self.mesh = PrismMesh.new()
+			if params[1].is_empty():
+				params[1] = [.5,[1,1,1],0,0,0]
+			self.mesh.left_to_right = params[1][0]
+			self.mesh.size = Vector3(params[1][1][0],params[1][1][1], params[1][1][2])
+			self.mesh.subdivide_width = params[1][2]
+			self.mesh.subdivide_height = params[1][3]
+			self.mesh.subdivide_depth = params[1][4]
 		"SphereMesh":
 			self.mesh = SphereMesh.new()
 			if params[1].is_empty():
@@ -55,8 +81,14 @@ func to_json():
 	match mesh.get_class():
 		"BoxMesh":
 			params = [[mesh.size.x, mesh.size.y, mesh.size.z], mesh.subdivide_width, mesh.subdivide_height, mesh.subdivide_depth]
+		"CapsuleMesh":
+			params = [mesh.radius, mesh.height, mesh.radial_segments, mesh.rings]
 		"CylinderMesh":
 			params = [mesh.top_radius, mesh.bottom_radius, mesh.height, mesh.radial_segments, mesh.rings, mesh.cap_top, mesh.cap_bottom]
+		"PlaneMesh":
+			params = [[mesh.size.x, mesh.size.y], mesh.subdivide_width, mesh.subdivide_depth, [mesh.center_offset.x, mesh.center_offset.y, mesh.center_offset.z], mesh.orientation]
+		"PrismMesh":
+			params = [mesh.left_to_right, [mesh.size.x, mesh.size.y, mesh.size.z], mesh.subdivide_width, mesh.subdivide_height, mesh.subdivide_depth]
 		"SphereMesh":
 			params = [mesh.radius, mesh.height, mesh.radial_segments, mesh.rings, mesh.is_hemisphere]
 		"TorusMesh":
